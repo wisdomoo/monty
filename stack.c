29f5879a18fd343c5e1b0d8f3f97e43246970142
wisdomoo <okoroaforwisdomo@gmail.com>
2022-12-14 13:08:40 +0100

    files uploaded

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - Prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
